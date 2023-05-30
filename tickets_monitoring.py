8bb14aab5c1bd5e45545e1d96af30ff5854105a6
RanaJT <127953290+RanaJT@users.noreply.github.com>
2023-05-30 17:06:06 +0100

    Update tickets_monitoring.py
    added exception handling, 
    prompt user input for CSS/URL, Emails and password instead of hardcoding.

--- tickets_monitoring.py
+++ tickets_monitoring.py
@@ -5,43 +5,57 @@ import time
 
 # Function to check ticket availability
 def check_tickets():
-    url = ' '  # Replace with the URL of the ticket site
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    
-    
-    # Check for specific elements indicating ticket availability
-    tickets_available = soup.select(' ')  # Replace with the CSS selector for available tickets
-    
-    print(f'tickets_available: {tickets_available}')  # Print the value of tickets_available
+    try:
 
-    if tickets_available:
-        send_notification()  # Call the function to send notification
-        print('Tickets are available!')
-    else:
-        print('Tickets are not available.')
+        response = requests.get(url)
+        response.raise_for_status()  # Raise an exception if the request fails
+        soup = BeautifulSoup(response.text, 'html.parser')
 
-    print('check_tickets() function called.')
+        # Check for specific elements indicating ticket availability
+        tickets_available = soup.select(css_selector)
 
+        print(f'tickets_available: {tickets_available}')  # Print the value of tickets_available
+
+        if tickets_available:
+            send_notification()  # Call the function to send notification
+            print('Tickets are available!')
+        else:
+            print('Tickets are not available.')
+    
+    except requests.exceptions.RequestException as e:
+        print(f'Request error: {e}')
+    except Exception as e:
+        print(f'An error has occured: {e}')
 
 # Function to send email notification
 def send_notification():
-    sender_email = ' '  # Replace with your email address
-    receiver_email = ' '  # Replace with recipient email address
-    password = ' '  # Replace with your email password
+            
+    try:
+        subject = 'Tickets are available!'
+        body = 'Tickets for the event are now available. Book your tickets as soon as possible!'
+        message = f'Subject: {subject}\n\n{body}'
+
+        with smtplib.SMTP('smtp.gmail.com', 587) as server:
+            server.starttls()
+            server.login(sender_email, password)
+            server.sendmail(sender_email, receiver_email, message)
     
-    subject = 'Tickets are available!'
-    body = 'Tickets for the event are now available. Book your tickets as soon as possible!'
-    message = f'Subject: {subject}\n\n{body}'
-    
-    with smtplib.SMTP('smtp.gmail.com', 587) as server:
-        server.starttls()
-        server.login(sender_email, password)
-        server.sendmail(sender_email, receiver_email, message)
-while True:
-    print('Starting a new iteration of the while loop...')
-    check_tickets()
-    print('After calling check_tickets()')
-    print('Waiting for the next check...')
-    time.sleep(5)
-    print('After waiting for 5 seconds')
+    except smtplib.SMTPException as e:
+        print(f'Failed to send email: {e}')
+    except Exception as e:
+        print(f'An error has occured: {e}')
+
+if __name__ == '__main__':
+    url = input('Enter the ticket site URL: \n>')
+    css_selector = input('Enter the tickets CSS selector: \n>')
+    sender_email = input('Enter your email address: \n>')
+    receiver_email = input('Enter the recipient email address: \n>')
+    password = input('Enter your email password: \n>')
+
+    while True:
+        print('Starting a new iteration of the while loop...')
+        check_tickets()
+        print('After calling check_tickets()')
+        print('Waiting for the next check...')
+        time.sleep(5)
+        print('After waiting for 5 seconds')
